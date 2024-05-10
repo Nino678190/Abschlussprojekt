3255a99f03a6b46a13847d70d1fb43a99d5f526b
Nino678190 <superwauzi@gmx.de>
2024-05-10 17:34:03 +0200

    Taschenrechner funktioniert

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ function displayValue(val) {
   document.getElementById("result").value += val;
 }
 
-// evaluates the digit and return result
 function solve() {
   let x = document.getElementById("result").value;
   let y = eval(x);
   document.getElementById("result").value = y;
 }
 
-// clear the result
 function clearResult() {
   document.getElementById("result").value = "";
 }
