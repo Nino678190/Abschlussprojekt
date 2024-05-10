0e1b3658c598afdf178f690db7e3ea42d7666957
Nino678190 <superwauzi@gmx.de>
2024-05-10 23:46:15 +0200

    Einfacher Verlauf hinzu

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ function solve() {
   let x = document.getElementById("result").value;
   let y = eval(x);
   document.getElementById("result").value = y;
+  // displayXandY();
 }
 
 function clearResult() {
   document.getElementById("result").value = "";
+}
+
+function verlauf() {
+  let z = document.getElementById("result").value;
+  solve()
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  let div = document.getElementById("verlauf"); 
+  div.innerHTML = `<p>${z} = ${y}</p>`;
+  document.body.appendChild(div);
 }
